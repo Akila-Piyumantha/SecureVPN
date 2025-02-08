c3f4ab26024a70d16e3b8074730be217f8016879
Akila-Piyumantha <akila7976ap@gmail.com>
2025-02-08 08:20:24 +0530

    a

---
+++ vpn/src/main/java/server/server.java
@@ -0,0 +1,11 @@
+package server;
+
+import java.net.ServerSocket;
+
+public class server {
+    private ServerSocket handShakeSocket;
+    private ServerSocket listenSocket;
+    private int targetPort;
+    
+
+}
